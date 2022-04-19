1f0ad4a715fd6f7c48fbe59dc77483e8d5e30e81
Daniel Dowling <danieldowling@Daniels-MBP-34.home>
2022-04-18 23:20:23 -0400

    Inital Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchBarComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule
